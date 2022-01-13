862eb29b34a4b9e748ddf9af83c037ad999d23a9
MickaelBlet <mickael.blet@gmail.com>
2022-01-14 00:57:24 +0100

    CMAKE_PREFIX_PATH in build.sh

--- multi_lib/build.sh
+++ multi_lib/build.sh
@@ -7,14 +7,14 @@ rm -rf build
 mkdir build
 pushd build
 cmake -DCMAKE_INSTALL_PREFIX="$(pwd)" -j ..
-make -j
+make VERBOSE=1 -j
 make install -j
 popd
 pushd test
 rm -rf build
 mkdir build
 pushd build
-cmake -DCMAKE_INSTALL_PREFIX="$(pwd)" -j ..
-make -j
+cmake -DCMAKE_FIND_DEBUG_MODE=ON -DCMAKE_INSTALL_PREFIX="$(pwd)" -DCMAKE_PREFIX_PATH="$(pwd)/../../build/lib" -j ..
+make VERBOSE=1 -j
 popd
 popd
